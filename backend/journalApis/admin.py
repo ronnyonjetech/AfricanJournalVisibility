c723d37b8110a59bffd6e9d5696467746b680221
ronnyonjetech <nyonjeron@gmail.com>
2024-08-05 13:02:57 +0300

    api documentation finished

--- backend/journalApis/admin.py
+++ backend/journalApis/admin.py
@@ -1,21 +1,40 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-# Register your models here.
+# # Register your models here.
 
-from .models import Journal, Volume, PDF
+# from .models import Journal, Volume, PDF
 
-class PDFInline(admin.TabularInline):
-    model = PDF
-    extra = 1  # Number of empty forms to display for adding PDFs
+# class PDFInline(admin.TabularInline):
+#     model = PDF
+#     extra = 1  # Number of empty forms to display for adding PDFs
 
-class VolumeAdmin(admin.ModelAdmin):
-    inlines = [PDFInline]  # Allow PDFs to be added inline with the Volume
+# class VolumeAdmin(admin.ModelAdmin):
+#     inlines = [PDFInline]  # Allow PDFs to be added inline with the Volume
     
 
-class JournalAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'doi', 'created_at')  # Columns to display in the admin list view
-    search_fields = ('name', 'author', 'doi')  # Searchable fields
+# class JournalAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'author', 'doi', 'created_at')  # Columns to display in the admin list view
+#     search_fields = ('name', 'author', 'doi')  # Searchable fields
 
-admin.site.register(Journal, JournalAdmin)
-admin.site.register(Volume, VolumeAdmin)
-admin.site.register(PDF)
+# admin.site.register(Journal, JournalAdmin)
+# admin.site.register(Volume, VolumeAdmin)
+# admin.site.register(PDF)
+from django.contrib import admin
+from .models import Journal, Volume, PDF
+
+@admin.register(Journal)
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')  # Columns to display in the list view
+    search_fields = ('name',)  # Add search functionality for the journal name
+
+@admin.register(Volume)
+class VolumeAdmin(admin.ModelAdmin):
+    list_display = ('journal', 'volume_number', 'created_at')  # Display volume number and journal
+    list_filter = ('journal',)  # Filter by journal in the admin interface
+
+@admin.register(PDF)
+class PDFAdmin(admin.ModelAdmin):
+    list_display = ('title', 'volume', 'author', 'doi', 'language', 'created_at')  # Display PDF details
+    search_fields = ('title', 'author', 'doi')  # Add search functionality for title, author, and DOI
+    list_filter = ('volume', 'language')  # Filter by volume and language
+
