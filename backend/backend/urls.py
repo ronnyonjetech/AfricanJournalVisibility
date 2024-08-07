337b1df6b9d5cebedc4103e7433a97376014df06
ronnyonjetech <nyonjeron@gmail.com>
2024-08-07 13:05:56 +0300

    latest update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView,SpectacularRedocView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('journal_api/',include('journalApis.urls')),
     path('api/',include('authApi.urls')),
     path('schema/',SpectacularAPIView.as_view(),name="schema"),
     path('',SpectacularSwaggerView.as_view(url_name='schema')),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),  
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
